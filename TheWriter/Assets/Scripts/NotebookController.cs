724848c97c860c11fbc221d650503db94af72f5e
KotoriYousan <kurosawa1997@gmail.com>
2020-08-31 21:48:48 -0400

    first push

---
+++ TheWriter/Assets/Scripts/NotebookController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NotebookController : MonoBehaviour
+{
+
+    public Text Notebook;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Notebook.text = "开始调查。";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "item")
+        {
+            Debug.Log("item triggered");
+            Notebook.text = Notebook.text + "\n捡起了" + other.gameObject.name + "。";
+        }
+    }
+}
