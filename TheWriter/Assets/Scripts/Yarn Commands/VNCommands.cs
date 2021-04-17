88a4bb252b877628add5c40d295cd4fa056c970d
KotoriYousan <kurosawa1997@gmail.com>
2021-04-17 15:32:25 -0400

    <add> show character avatar when speaking

--- TheWriter/Assets/Scripts/Yarn Commands/VNCommands.cs
+++ TheWriter/Assets/Scripts/Yarn Commands/VNCommands.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Yarn.Unity;
 
 public class VNCommands : MonoBehaviour
@@ -11,6 +12,7 @@ public class VNCommands : MonoBehaviour
         Debug.Log("yarn command run");
 
         gameObject.GetComponent<SpriteRenderer>().enabled = true;
+        //gameObject.SetActive(true);
     }
 
     [YarnCommand("Hide")]
@@ -19,6 +21,32 @@ public class VNCommands : MonoBehaviour
         Debug.Log("yarn command run");
 
         gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        //gameObject.SetActive(false);
+    }
+
+
+    public Sprite gin, mian;
+    [YarnCommand("SetAvatar")]
+    public void SetAvatar(string chname)
+    {
+        Debug.Log("yarn command run");
+        
+
+        if(chname == "gin")
+        {
+            gameObject.GetComponent<Image>().enabled = true;
+            gameObject.GetComponent<Image>().sprite = gin;
+            //gameObject.GetComponent<SpriteRenderer>().sprite = gin;
+        }else if(chname == "mian")
+        {
+            gameObject.GetComponent<Image>().enabled = true;
+            gameObject.GetComponent<Image>().sprite = mian;
+            //gameObject.GetComponent<SpriteRenderer>().sprite = mian;
+        }else if(chname == "none")
+        {
+            gameObject.GetComponent<Image>().enabled = false;
+            //gameObject.GetComponent<SpriteRenderer>().sprite = null;
+        }
     }
 
 }
