43dd4fbe4af83cf7a0583c1742d96e2faa8f2acb
KotoriYousan <kurosawa1997@gmail.com>
2021-04-20 12:36:39 -0400

    <add> VO for prologue ad

---
+++ TheWriter/Assets/Scripts/Yarn Commands/PlaySoundCommand.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Yarn.Unity;
+
+public class PlaySoundCommand : MonoBehaviour
+{
+
+    public AudioSource audioSource;
+    public AudioClip[] ads;
+
+
+    [YarnCommand("PlaySound")]
+    public void PlaySound(string clipname)
+    {
+
+        foreach (AudioClip clip in ads)
+        {
+            if (clip.name == clipname)
+                //audioSource.Stop();
+                audioSource.clip = clip;
+                audioSource.Play();
+        }
+
+        //audioSource.clip = clip;
+        //audioSource.Play();
+    }
+
+
+}
