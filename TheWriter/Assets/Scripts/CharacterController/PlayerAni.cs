62d6ae66a5a5050571874bf893ceaf418adf687f
KotoriYousan <kurosawa1997@gmail.com>
2021-04-22 20:21:41 -0400

    <add> Gu gugu first before I start to write walking anim

---
+++ TheWriter/Assets/Scripts/CharacterController/PlayerAni.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAni : MonoBehaviour
+{
+    private Animator anim;
+    //public Transform roadAxis;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        //Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
+
+        bool moving = false;
+        if (Input.GetKey(KeyCode.W))
+        {
+            anim.SetBool("isWalking", true);
+            anim.SetBool("isWalkingBack", false);
+            anim.SetBool("isWalkingRight", false);
+            anim.SetBool("isWalkingLeft", false);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            anim.SetBool("isWalkingRight", true);
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isWalkingBack", false);
+            anim.SetBool("isWalkingLeft", false);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            anim.SetBool("isWalkingLeft", true);
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isWalkingRight", false);
+            anim.SetBool("isWalkingBack", false);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            anim.SetBool("isWalkingBack", true);
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isWalkingRight", false);
+            anim.SetBool("isWalkingLeft", false);
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isWalkingBack", false);
+            anim.SetBool("isWalkingRight", false);
+            anim.SetBool("isWalkingLeft", false);
+        }
+    }
+}
